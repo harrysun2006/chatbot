ffa63476b304df3e08f7e20f6d15706ccbf45757
Harry Sun <hongyu.sun@anz.com>
2018-02-02 15:26:08 +1100

    check balance action

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,33 @@
 
 const functions = require('firebase-functions');
 const DialogflowApp = require('actions-on-google').DialogflowApp;
+const validUsers = ['Harry', 'hiten', 'Liu'];
+const balances = {
+	'Harry': 'You have $800 in your bank account!',
+	'hiten': 'You have $1000 in your savings account ending with one four four four and $1200 in your savings account ending with six seven eight nine',
+	'Liu': 'You have $1200 in your CBA bank account, $700 in NAB bank account and $900 in ANZ bank account',
+};
+const users = {
+	'ABwppHHbUczQE7IbzOQ0RgiNi1A9yAPh9TC3Tna-04MjcRcXoFYPmN891NJXnIik75PyPH-09-j9': {
+		'givenName': 'Harry'
+	},
+	'ABwppHEsBEwccCD1KKzw7zutnhLcTBIgVsAQ4iGAeAmgYYoJEjE_DAY8afVJyfJny1RHLFCXlpWl': {
+		'givenName': 'hiten'
+	},
+	'ABwppHGTr6dwp5CKjbhwjxFEP-Ir2XFUu04Ft_cOFbPXj_mqvbdeI5L22YKUvQ55fHovYl4afU0w': {
+		'givenName': 'Liu'
+	}
+};
+var user;
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     
 	const app = new DialogflowApp({request: request, response: response});
-	const REQUEST_PERMISSION_ACTION = 'request_permission';
-	const SAY_NAME_ACTION = 'get_name';
+	const REQUEST_PERMISSION_ACTION = 'input.welcome';
+	const SAY_NAME_ACTION = 'input.welcome'; // 'get_name';
+	const BALANCE_CHECK = 'account.balance.check';
 	const userId = app.getUser().userId;
+
 
 	function requestPermission (app) {
 	  const permission = app.SupportedPermissions.NAME;
@@ -16,17 +36,34 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 	}
 
 	function sayName (app) {
+  	console.log('Uer Id: ' + userId);
 	  if (app.isPermissionGranted()) {
-	  	console.log('Uer Id: ' + userId);
-	    app.tell('Welcome to Cognizant Bank, ' + app.getUserName().givenName);
+	  	user = app.getUserName().givenName;
 	  } else {
 	    // Response shows that user did not grant permission
-	    app.tell('Sorry, I could not get your name.');
-	  }
+	    // app.ask('Sorry, I could not get your name.');
+	    user = users[userId].givenName;
+	  }  	
+    app.ask('Hi ' + user + ', welcome to CognoBot! What can I do for you?');
 	}
+
+	function sayBalance (app) {
+		// var text = 'You have ' + balances[user] + ' in your bank account!';
+		var text = balances[user];
+		console.log(text);
+		app.ask(text);
+	}
+
+	function sayTest (app) {
+		app.ask("I'm here!");
+	}
+
 	const actionMap = new Map();
-	actionMap.set(REQUEST_PERMISSION_ACTION, requestPermission);
+	// actionMap.set(REQUEST_PERMISSION_ACTION, requestPermission);
 	actionMap.set(SAY_NAME_ACTION, sayName);
+	actionMap.set(BALANCE_CHECK, sayBalance);
+	actionMap.set('test.firebase', sayTest);
 	app.handleRequest(actionMap);
 
 });
+
